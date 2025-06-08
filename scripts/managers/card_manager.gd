f7d24b1306635c020c14d3b8b6546bc7d59f65b2
0600dev <0600dev@gmail.com>
2025-06-08 01:18:24 -0500

    fix: dragging and hover effect

--- scripts/managers/card_manager.gd
+++ scripts/managers/card_manager.gd
@@ -2,59 +2,34 @@ extends Node2D
 #
 var card_being_dragged
 var screen_size
-var is_hovering_on_card: bool
-# consts
-const COLLISION_MASK_CARD = 1
-const COLLISION_MASK_SLOT = 2
-# refs
-var player_hand_ref
+var is_hovering_on_card
+
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	#
-	player_hand_ref = $"../player_hand"
-	print(player_hand_ref.get_class())
 
-
-func _process(_delta: float) -> void:
+	
+func _process(delta: float) -> void:
 	if card_being_dragged:
 		var mouse_pos = get_global_mouse_position()
 		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x),
-			clamp(mouse_pos.y, 0, screen_size.y))
-		
+		clamp(mouse_pos.y, 0, screen_size.y))
 
-func _input(event: InputEvent) -> void:
+func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			var card = raycast_check_for_card()
 			if card:
-				start_drag(card)
+				card_being_dragged = card
 		else:
-			finish_drag()
-			
-			
-func start_drag(card):
-	card.scale = Vector2(1, 1)	
-	card_being_dragged = card
-	
-	
-func finish_drag():
-	card_being_dragged.scale = Vector2(1.1, 1.1)
-	var card_slot_found = raycast_check_for_slot()
-	if card_slot_found and not card_slot_found.card_in_slot:
-		card_being_dragged.position = card_slot_found.position
-		#card_slot_found.get_node("Area2D/CollisionShape2D").disabled = true
-	else:
-		player_hand_ref.add_card_to_hand(card_being_dragged)
-	card_being_dragged = null
-	
-	
-			
+			card_being_dragged = null
+
+
 func connect_card_signals(card):
 	card.connect("hovered", on_hovered_over_card)
 	card.connect("hovered_off", on_hovered_off_card)
-	
-	
+
+
 func on_hovered_over_card(card):
 	if !is_hovering_on_card:
 		is_hovering_on_card = true
@@ -68,8 +43,7 @@ func on_hovered_off_card(card):
 		highlight_card(new_card_hovered, true)
 	else:
 		is_hovering_on_card = false
-	
-	
+
 func highlight_card(card, hovered):
 	if hovered:
 		card.scale = Vector2(1.1, 1.1)
@@ -77,41 +51,32 @@ func highlight_card(card, hovered):
 	else:
 		card.scale = Vector2(1, 1)
 		card.z_index = 1	
-	
-			
+
+
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
 	var parameters = PhysicsPointQueryParameters2D.new()
 	parameters.position = get_global_mouse_position()
 	parameters.collide_with_areas = true
-	parameters.collision_mask = COLLISION_MASK_CARD
+	parameters.collision_mask = 1
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
 		return get_card_with_highest_z_index(result)
 	return null
 	
-	
-func raycast_check_for_slot():
-	var space_state = get_world_2d().direct_space_state
-	var parameters = PhysicsPointQueryParameters2D.new()
-	parameters.position = get_global_mouse_position()
-	parameters.collide_with_areas = true
-	parameters.collision_mask = COLLISION_MASK_SLOT
-	var result = space_state.intersect_point(parameters)
-	if result.size() > 0:
-		return result[0].collider.get_parent()
-	return null
-	
-	
+
 func get_card_with_highest_z_index(cards):
 	var highest_z_card = cards[0].collider.get_parent()
 	var highest_z_index = highest_z_card.z_index
-	# 
 	for i in range(1, cards.size()):
 		var current_card = cards[i].collider.get_parent()
 		if current_card.z_index > highest_z_index:
 			highest_z_card = current_card
 			highest_z_index = current_card.z_index
 	return highest_z_card
+	
+	
+
+	
 	
 	
--- scripts/objects/card.gd
+++ scripts/objects/card.gd
@@ -1,19 +1,23 @@
 extends Node2D
-# signals
+
 signal hovered
 signal hovered_off
-#
-var hand_pos
-#
+
 
 func _ready() -> void:
+	self.reparent($"..")
+	print(get_parent())
 	get_parent().connect_card_signals(self)
+
 	
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
 
-
+	
+	
 func _on_area_2d_mouse_entered() -> void:
 	emit_signal("hovered", self)
+
+
 func _on_area_2d_mouse_exited() -> void:
 	emit_signal("hovered_off", self)
