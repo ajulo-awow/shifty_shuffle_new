54bb5e52c2a94f7cdd65284827d302136d570816
0600dev <0600dev@gmail.com>
2025-06-08 01:55:59 -0500

    fix: card slots
    able to pick card out of card slot

--- scripts/managers/card_manager.gd
+++ scripts/managers/card_manager.gd
@@ -3,6 +3,9 @@ extends Node2D
 var card_being_dragged
 var screen_size
 var is_hovering_on_card
+#
+const COLL_MASK_CARD = 1
+const COLL_MASK_CARD_SLOT = 2
 
 
 func _ready() -> void:
@@ -34,8 +37,18 @@ func start_drag(card):
 
 func finish_drag():
 	card_being_dragged.scale = Vector2(1.1, 1.1)
-	card_being_dragged = card_being_dragged
+	# if card dropped in empty card slot
+	var card_slot_found = raycast_check_for_card_slot()
+	if card_slot_found and not card_slot_found.card_in_slot:
+		card_being_dragged.position = card_slot_found.position
+		# disable card
+		#card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
+		#card_slot_found.card_in_slot = true
+		# card not bound to card slot
+		card_slot_found.card_in_slot = false
+	#
 	card_being_dragged = null
+
 
 func connect_card_signals(card):
 	card.connect("hovered", on_hovered_over_card)
@@ -50,11 +63,13 @@ func on_hovered_over_card(card):
 	
 func on_hovered_off_card(card):
 	highlight_card(card, false)
+	#
 	var new_card_hovered = raycast_check_for_card()
 	if new_card_hovered:
 		highlight_card(new_card_hovered, true)
 	else:
 		is_hovering_on_card = false
+
 
 func highlight_card(card, hovered):
 	if hovered:
@@ -66,20 +81,36 @@ func highlight_card(card, hovered):
 
 
 func raycast_check_for_card():
+	#
 	var space_state = get_world_2d().direct_space_state
 	var parameters = PhysicsPointQueryParameters2D.new()
 	parameters.position = get_global_mouse_position()
 	parameters.collide_with_areas = true
-	parameters.collision_mask = 1
+	parameters.collision_mask = COLL_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
 		return get_card_with_highest_z_index(result)
 	return null
 	
+	
+func raycast_check_for_card_slot():
+	#
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLL_MASK_CARD_SLOT
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		return result[0].collider.get_parent()
+	return null
+	
 
 func get_card_with_highest_z_index(cards):
+	#
 	var highest_z_card = cards[0].collider.get_parent()
 	var highest_z_index = highest_z_card.z_index
+	#
 	for i in range(1, cards.size()):
 		var current_card = cards[i].collider.get_parent()
 		if current_card.z_index > highest_z_index:
--- scripts/objects/slot.gd
+++ scripts/objects/slot.gd
@@ -1,11 +1,3 @@
 extends Node2D
 #
 var card_in_slot: bool = false
-
-
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-func _process(_delta: float) -> void:
-	pass
