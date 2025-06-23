2822f16c01f80ae1c5fcd5f1fb9cbd2cd095c0b9
0600dev <0600dev@gmail.com>
2025-06-23 13:29:36 -0500

    fix: start menu art

--- scripts/menus/start_menu.gd
+++ scripts/menus/start_menu.gd
@@ -1,4 +1,17 @@
 extends Control
+#
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var start_button: TextureButton = $start
+@onready var settings_button: TextureButton = $settings
+@onready var credits_button: TextureButton = $credits
+
+
+func _ready() -> void:
+	anim_player.play("start_menu_title_anim")
+	#
+	start_button.pivot_offset = start_button.size/2
+	settings_button.pivot_offset = settings_button.size/2
+	credits_button.pivot_offset = credits_button.size/2
 
 
 func _on_start_pressed() -> void:
@@ -11,3 +24,21 @@ func _on_settings_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/credits_menu.tscn")
+
+
+func _on_start_mouse_entered() -> void:
+	start_button.scale = Vector2(1.6, 1.6)
+func _on_start_mouse_exited() -> void:
+	start_button.scale = Vector2(1.5, 1.5)
+
+
+func _on_settings_mouse_entered() -> void:
+	settings_button.scale = Vector2(1.6, 1.6)
+func _on_settings_mouse_exited() -> void:
+	settings_button.scale = Vector2(1.5, 1.5)
+	
+
+func _on_credits_mouse_entered() -> void:
+	credits_button.scale = Vector2(1.6, 1.6)
+func _on_credits_mouse_exited() -> void:
+	credits_button.scale = Vector2(1.5, 1.5)
